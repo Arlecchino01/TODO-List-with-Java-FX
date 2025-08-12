1c73a377e8039d98c3c351e3be93bddb0bbdcc3e
nZero <damun902@gmail.com>
2025-08-12 20:43:03 +0900

    ad

--- Main.java
+++ Main.java
@@ -5,17 +5,25 @@ import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
 
 
 public class Main extends Application{
-
+    
     @Override
-    public void start(Stage stage){
-        //빈 루트 컨테이너
-        StackPane root = new StackPane();
+    public void start(Stage stage) throws Exception{
+        Font.loadFont(getClass().getResourceAsStream("/resources/fonts/Monoton.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("/resources/fonts/Poppins-Black.ttf"), 14);
+        Label label = new Label("Font test");
+        
+        //루트 컨테이너
+        StackPane root = new StackPane(label);
 
         //Scene 생성
         Scene scene = new Scene(root, 700, 400);
+
+        scene.getStylesheets().add(getClass().getResource("resources/style.css").toExternalForm());
 
         //Stage 설정
         stage.setTitle("FX 테스팅");
